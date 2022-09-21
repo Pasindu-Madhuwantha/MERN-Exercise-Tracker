db2c95151ab9a0ab6e08b8d53e6ecb9fba54796e
PASINDU999 <mpasindu72@gmail.com>
2022-09-21 19:24:35 +0530

    first commit

---
+++ mern-exercise-tracker/src/components/create-exercise.component.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+export default class CreateExercise extends Component{
+render(){
+    return(
+        <div>
+            <p>You are Here CreateUsers</p>
+        </div>
+    )
+
+}
+
+}
---
+++ mern-exercise-tracker/src/components/create-user.component.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+export default class CreateUser extends Component{
+render(){
+    return(
+        <div>
+            <p>You are Here CreateUsers</p>
+        </div>
+    )
+
+}
+
+}
