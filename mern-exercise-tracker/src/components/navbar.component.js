4d06ebb868058acbd80ad664bb154eccf4d21201
Pasindu-Madhuwantha <mpasindu72@gmail.com>
2023-01-05 12:46:52 +0530

    corrrected the update

--- mern-exercise-tracker/src/components/navbar.component.js
+++ mern-exercise-tracker/src/components/navbar.component.js
@@ -1,6 +1,5 @@
 import React, { Component } from "react";
-import { Link } from "react-router-dom";
-
+import { Outlet, Link } from "react-router-dom";
 
 
 
@@ -8,22 +7,43 @@ import { Link } from "react-router-dom";
     export default class NavBar extends Component {
         render() {
           return (
-            <nav className="navbar navbar-expand-xl bg-primary navbar-dark pr-lg-1 pl-sm-5 justify-content-between">
+        /*     <nav className="navbar navbar-dark bg-dark justify-content-between">
               <Link to="/" className="navbar-brand">ExcerTracker</Link>
               <div className="collapse navbar-collapse">
                 <ul className="navbar-nav mr-auto">
-                  <li className="navbar-item">
+                  <li className="nav-item">
                     <Link to="/" className="nav-link">Exercises</Link>
                   </li>
-                  <li className="navbar-item">
+                  <li className="nav-item">
                     <Link to="/create" className="nav-link">Create Exercise Log</Link>
                   </li>
-                  <li className="navbar-item">
+                  <li className="nav-item">
                     <Link to="/user" className="nav-link">Create User</Link>
                   </li>
                 </ul>
                 </div>
             </nav>
+ */
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+<Link to="/" className="navbar-brand">ExcerTracker</Link>  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarText">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+      <Link to="/" className="nav-link">Exercises</Link>
+      </li>
+      <li class="nav-item">
+      <Link to="/create" className="nav-link">Create Exercise Log</Link>
+      </li>
+      <li class="nav-item">
+      <Link to="/user" className="nav-link">Create User</Link>
+      </li>
+    </ul>
+  
+  </div>
+</nav>
+            
       
           );
         }
